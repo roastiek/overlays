6c3961972991590542a9bcba4d72fdcccaf2f9a2
Rostislav Benes <rostislav.benes@firma.seznam.cz>
2026-05-17 17:05:07 +0200

    devens update

--- devenvs/python-overlay.nix
+++ devenvs/python-overlay.nix
@@ -1,18 +1,30 @@
 self: super: {
-  uv-fhs = let
-    uv = self.buildFHSEnv {
-      inherit (super.uv) pname version;
-      targetPkgs = pkgs: [ pkgs.stdenv.cc.cc.lib ];
-      runScript = "${super.uv}/bin/uv";
+  uv-fhs =
+    let
+      uv = self.buildFHSEnv {
+        inherit (super.uv) pname version;
+        targetPkgs = pkgs: [
+          pkgs.stdenv.cc.cc.lib
+          pkgs.libz
+        ];
+        runScript = "${super.uv}/bin/uv";
+      };
+      uvx = self.buildFHSEnv {
+        inherit (super.uv) pname version;
+        targetPkgs = pkgs: [
+          pkgs.stdenv.cc.cc.lib
+          pkgs.libz
+        ];
+        runScript = "${super.uv}/bin/uvx";
+        executableName = "uvx";
+      };
+    in
+    self.symlinkJoin {
+      inherit (super.uv) name pname version;
+      paths = [
+        uv
+        uvx
+        super.uv
+      ];
     };
-    uvx = self.buildFHSEnv {
-      inherit (super.uv) pname version;
-      targetPkgs = pkgs: [ pkgs.stdenv.cc.cc.lib ];
-      runScript = "${super.uv}/bin/uvx";
-      executableName = "uvx";
-    };
-  in self.symlinkJoin {
-    inherit (super.uv) name pname version;
-    paths = [ uv uvx super.uv ];
-  };
 }
